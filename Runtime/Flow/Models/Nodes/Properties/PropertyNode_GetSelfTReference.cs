15129c37e7344a7246ddfa6f173fc02c01c1fac4
AkiKurisu <2683987717@qq.com>
2024-12-31 01:08:22 +0800

    Add GetSelfTReference

---
+++ Runtime/Flow/Models/Nodes/Properties/PropertyNode_GetSelfTReference.cs
@@ -0,0 +1,21 @@
+using System;
+using Ceres.Annotations;
+using Cysharp.Threading.Tasks;
+using UObject = UnityEngine.Object;
+namespace Ceres.Graph.Flow.Properties
+{
+    [Serializable]
+    [NodeGroup("Hidden")]
+    [CeresLabel("{0}")]
+    public class PropertyNode_GetSelfTReference<TTarget>: PropertyNode where TTarget: UObject
+    {
+        [OutputPort, CeresLabel("Value")]
+        public CeresPort<TTarget> outputValue;
+        
+        protected override UniTask Execute(ExecutionContext executionContext)
+        {
+            outputValue.Value = (TTarget)executionContext.Context;
+            return UniTask.CompletedTask;
+        }
+    }
+}
