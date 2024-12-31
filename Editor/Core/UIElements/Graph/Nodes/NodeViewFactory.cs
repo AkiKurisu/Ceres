579f4cce8ecba11eccd77d156cb47c49fe359c14
AkiKurisu <2683987717@qq.com>
2024-12-31 13:27:01 +0800

    Update INodeViewResolver
    Use instance method to accept node type

--- Editor/Core/UIElements/Graph/Nodes/NodeViewFactory.cs
+++ Editor/Core/UIElements/Graph/Nodes/NodeViewFactory.cs
@@ -10,10 +10,40 @@ namespace Ceres.Editor.Graph
     public interface INodeViewResolver
     {
         ICeresNodeView CreateNodeView(Type type, CeresGraphView graphView);
+
+        bool IsAcceptable(Type nodeType);
     }
 
     public sealed class NodeViewFactory
     {
+        public class ResolverStructure
+        {
+            public readonly Type Type;
+            
+            public readonly INodeViewResolver Instance;
+
+            public readonly CustomNodeViewAttribute CustomNodeViewAttribute;
+            
+            public ResolverStructure(Type type)
+            {
+                Type = type;
+                if (type.GetInterfaces().Any(t => t == typeof(INodeViewResolver)))
+                {
+                    Instance = (INodeViewResolver)Activator.CreateInstance(type);
+                }
+                else
+                {
+                    CustomNodeViewAttribute = type.GetCustomAttribute<CustomNodeViewAttribute>();
+                }
+            }
+
+            public bool IsAcceptable(Type inType)
+            {
+                if (Instance == null) return false;
+                return Instance.IsAcceptable(inType);
+            }
+        }
+        
         private static NodeViewFactory _default;
 
         public static NodeViewFactory Get()
@@ -21,24 +51,25 @@ namespace Ceres.Editor.Graph
             return _default ??= new NodeViewFactory();
         }
         
-        private readonly List<Type> _resolverTypes;
+        private readonly List<ResolverStructure> _resolvers;
 
         private NodeViewFactory()
         {
-            _resolverTypes = AppDomain.CurrentDomain
+            _resolvers = AppDomain.CurrentDomain
                                         .GetAssemblies()
                                         .Select(x => x.GetTypes())
                                         .SelectMany(x => x)
                                         .Where(IsValidType)
+                                        .Select(x=> new ResolverStructure(x))
                                         .ToList();
-            _resolverTypes.Sort((a, b) =>
+            _resolvers.Sort((a, b) =>
             {
-                var aCustom = a.GetCustomAttribute<CustomNodeViewAttribute>(false);
-                var bCustom = b.GetCustomAttribute<CustomNodeViewAttribute>(false);
-                var aOrdered = a.GetCustomAttribute<OrderedAttribute>(false);
-                var bOrdered = b.GetCustomAttribute<OrderedAttribute>(false);
-                if (aCustom == null && bCustom != null) return 1;
-                if (aCustom != null && bCustom == null) return -1;
+                var aCustom = a.Instance == null;
+                var bCustom = b.Instance == null;
+                var aOrdered = a.Type.GetCustomAttribute<OrderedAttribute>(false);
+                var bOrdered = b.Type.GetCustomAttribute<OrderedAttribute>(false);
+                if (!aCustom && bCustom) return 1;
+                if (aCustom && !bCustom) return -1;
                 if (aOrdered == null && bOrdered == null) return 0;
                 if (aOrdered != null && bOrdered != null) return aOrdered.Order - bOrdered.Order;
                 if (aOrdered != null) return -1;
@@ -62,21 +93,21 @@ namespace Ceres.Editor.Graph
         public ICeresNodeView CreateInstance(Type type, CeresGraphView graphView)
         {
             ICeresNodeView node = null;
-            foreach (var resolverType in _resolverTypes)
+            foreach (var resolver in _resolvers)
             {
-                var attribute = resolverType.GetCustomAttribute<CustomNodeViewAttribute>();
+                var attribute = resolver.CustomNodeViewAttribute;
                 if (attribute != null)
                 {
                     if (TryAcceptNodeEditor(attribute, type))
                     {
                         /* Must have (Type, CeresGraphView) constructor */
-                        node = (ICeresNodeView)Activator.CreateInstance(resolverType, type, graphView);
+                        node = (ICeresNodeView)Activator.CreateInstance(resolver.Type, type, graphView);
                         break;
                     }
                     continue;
                 }
-                if (!IsAcceptable(resolverType, type)) continue;
-                node = ((INodeViewResolver)Activator.CreateInstance(resolverType)).CreateNodeView(type, graphView);
+                if (!resolver.IsAcceptable(type)) continue;
+                node = ((INodeViewResolver)Activator.CreateInstance(resolver.Type)).CreateNodeView(type, graphView);
                 break;
             }
             return node;
@@ -92,17 +123,17 @@ namespace Ceres.Editor.Graph
         public ICeresNodeView CreateInstanceResolved(Type type, CeresGraphView graphView, params Type[] genericArguments)
         {
             ICeresNodeView node = null;
-            foreach (var resolverType in _resolverTypes)
+            foreach (var resolver in _resolvers)
             {
-                var attribute = resolverType.GetCustomAttribute<CustomNodeViewAttribute>();
+                var attribute = resolver.CustomNodeViewAttribute;
                 if (attribute != null)
                 {
                     if (TryAcceptNodeEditor(attribute, type))
                     {
-                        var viewType = resolverType;
+                        var viewType = resolver.Type;
                         if (viewType.IsGenericType)
                         {
-                            viewType = resolverType.MakeGenericType(genericArguments);
+                            viewType = viewType.MakeGenericType(genericArguments);
                             node = (ICeresNodeView)Activator.CreateInstance(viewType, type, graphView);
                         }
                         else
@@ -122,20 +153,17 @@ namespace Ceres.Editor.Graph
                 {
                     type = type.MakeGenericType(genericArguments);
                 }
-                if (!IsAcceptable(resolverType, type)) continue;
-                node = ((INodeViewResolver)Activator.CreateInstance(resolverType)).CreateNodeView(type, graphView);
+                if (!resolver.IsAcceptable(type)) continue;
+                node = ((INodeViewResolver)Activator.CreateInstance(resolver.Type)).CreateNodeView(type, graphView);
                 break;
             }
             return node;
         }
+        
         private static bool TryAcceptNodeEditor(CustomNodeViewAttribute attribute, Type nodeType)
         {
             if (attribute.NodeType == nodeType) return true;
             return attribute.CanInherit && attribute.NodeType.IsAssignableFrom(nodeType);
-        }
-        private static bool IsAcceptable(Type type, Type behaviorType)
-        {
-            return (bool)type.InvokeMember("IsAcceptable", BindingFlags.InvokeMethod, null, null, new object[] { behaviorType });
         }
     }
 }
--- Editor/Flow/Nodes/ForwardNodeViewResolver.cs
+++ Editor/Flow/Nodes/ForwardNodeViewResolver.cs
@@ -4,7 +4,11 @@ namespace Ceres.Editor.Graph.Flow
 {
     public class ForwardNodeViewResolver : INodeViewResolver
     {
-        public static bool IsAcceptable(Type nodeType) => nodeType.IsSubclassOf(typeof(ForwardNode));
+        public bool IsAcceptable(Type nodeType)
+        {
+            return nodeType.IsSubclassOf(typeof(ForwardNode));
+        }
+
         public ICeresNodeView CreateNodeView(Type type, CeresGraphView graphView)
         {
             return new ExecutableNodeView(type, graphView);
