d1eec8c5ffa5e32af747e5cf716bdf94451f2fe5
AkiKurisu <2683987717@qq.com>
2025-01-15 20:28:45 +0800

    Update FlowNode_InstantiateT.cs
    Complete the node input

--- Runtime/Flow/Models/Nodes/Utilities/FlowNode_InstantiateT.cs
+++ Runtime/Flow/Models/Nodes/Utilities/FlowNode_InstantiateT.cs
@@ -1,5 +1,6 @@
 using System;
 using Ceres.Annotations;
+using UnityEngine;
 using UObject = UnityEngine.Object;
 namespace Ceres.Graph.Flow.Utilities
 {
@@ -8,15 +9,34 @@ namespace Ceres.Graph.Flow.Utilities
     [CeresLabel("Instantiate {0}")]
     public class FlowNode_InstantiateT<TObject>: FlowNode where TObject: UObject
     {
-        [InputPort, HideInGraphEditor]
+        [InputPort]
         public CeresPort<TObject> original;
+        
+        [InputPort]
+        public CeresPort<Vector3> position;
+        
+        [InputPort]
+        public CeresPort<Quaternion> rotation;
+        
+        [InputPort]
+        public CeresPort<Transform> parent;
+        
+        [InputPort]
+        public CeresPort<bool> worldPositionStays;
                 
         [OutputPort, CeresLabel("Result")]
         public CeresPort<TObject> resultValue;
 
         protected override void LocalExecute(ExecutionContext executionContext)
         {
-            resultValue.Value = UObject.Instantiate(original.Value);
+            if (worldPositionStays.Value)
+            {
+                resultValue.Value = UObject.Instantiate(original.Value, parent.Value, worldPositionStays.Value);
+            }
+            else
+            {
+                resultValue.Value = UObject.Instantiate(original.Value, position.Value, rotation.Value, parent.Value);
+            }
         }
     }
 }
