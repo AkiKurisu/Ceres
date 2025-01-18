183f2547b122b51d244d623914814be02d857230
AkiKurisu <2683987717@qq.com>
2025-01-18 18:00:31 +0800

    Fix direct cast exception when SharedObject not set value

--- Runtime/Flow/Models/Nodes/Properties/PropertyNode_GetSharedVariableTValue.cs
+++ Runtime/Flow/Models/Nodes/Properties/PropertyNode_GetSharedVariableTValue.cs
@@ -16,12 +16,24 @@ namespace Ceres.Graph.Flow.Properties
     {
         [OutputPort, CeresLabel("Value")]
         public CeresPort<TOutValue> outputValue;
+
+        private static bool _isValueType;
         
         protected override UniTask Execute(ExecutionContext executionContext)
         {
-            if (executionContext.Graph.BlackBoard.GetSharedVariable(propertyName) is T variable) 
-                outputValue.Value = (TOutValue)variable.Value;
+            if (executionContext.Graph.BlackBoard.GetSharedVariable(propertyName) is T variable)
+            {
+                if(!_isValueType || variable.Value != null)
+                {
+                    outputValue.Value = (TOutValue)variable.Value;
+                }
+            }
             return UniTask.CompletedTask;
+        }
+
+        static PropertyNode_GetSharedVariableTValue()
+        {
+            _isValueType = typeof(TOutValue).IsValueType;
         }
     }
 }
