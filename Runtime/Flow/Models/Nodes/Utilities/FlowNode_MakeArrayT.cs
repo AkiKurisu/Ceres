a59059013bd70cc2dbadd3e747f5ca2da67f5b70
AkiKurisu <2683987717@qq.com>
2025-02-01 22:09:47 +0800

    Add FlowNode_MakeArrayT

---
+++ Runtime/Flow/Models/Nodes/Utilities/FlowNode_MakeArrayT.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Ceres.Annotations;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Ceres.Graph.Flow.Utilities
+{
+    public abstract class FlowNode_MakeArray : ExecutableNode
+    {
+    }
+    
+    [Serializable]
+    [CeresGroup("Utilities/Array")]
+    [CeresLabel("Make {0} Array")]
+    [CeresMetadata("style = ConstNode", "path = Dependency")]
+    public class FlowNode_MakeArrayT<T>: FlowNode_MakeArray, ISerializationCallbackReceiver, IPortArrayNode
+    {
+        [InputPort, CeresMetadata("DefaultLength = 1")]
+        public CeresPort<T>[] items;
+        
+        [HideInGraphEditor]
+        public int inputCount;
+        
+        [OutputPort]
+        public CeresPort<T[]> array;
+
+        protected sealed override UniTask Execute(ExecutionContext executionContext)
+        {
+            for (int i = 0; i < inputCount; i++)
+            {
+                array.Value[i] = items[i].Value;
+            }
+            return UniTask.CompletedTask;
+        }
+
+        public void OnBeforeSerialize()
+        {
+            
+        }
+
+        public void OnAfterDeserialize()
+        {
+            array.Value = new T[inputCount];
+        }
+
+        public int GetPortArrayLength()
+        {
+            return inputCount;
+        }
+
+        public string GetPortArrayFieldName()
+        {
+            return nameof(items);
+        }
+
+        public void SetPortArrayLength(int newLength)
+        {
+            inputCount = newLength;
+            items = new CeresPort<T>[inputCount];
+            for (int i = 0; i < newLength; i++)
+            {
+                items[i] = new CeresPort<T>();
+            }
+        }
+    }
+}
