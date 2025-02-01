da45b6d9a683b9e70b249254a7714044a99e0f2c
AkiKurisu <2683987717@qq.com>
2025-02-01 22:22:16 +0800

    Add TextExecutableLibrary

--- Runtime/Flow/Utilities/Libraries/TextExecutableLibrary.cs
+++ Runtime/Flow/Utilities/Libraries/TextExecutableLibrary.cs
@@ -1,5 +1,7 @@
+using System.Globalization;
 using Ceres.Annotations;
 using Ceres.Graph.Flow.Annotations;
+using Chris;
 
 namespace Ceres.Graph.Flow.Utilities
 {
@@ -9,16 +11,46 @@ namespace Ceres.Graph.Flow.Utilities
     [CeresGroup("Text")]
     public partial class TextExecutableLibrary : ExecutableFunctionLibrary
     {
-        [ExecutableFunction, CeresLabel("String Concat")]
+        [ExecutableFunction(ExecuteInDependency = true), CeresLabel("Concat String")]
         public static string Flow_StringConcat(string[] values)
         {
             return string.Concat(values);
         }
         
-        [ExecutableFunction, CeresLabel("String Join")]
+        [ExecutableFunction(ExecuteInDependency = true), CeresLabel("Join String")]
         public static string Flow_StringJoin(string separator, string[] values)
         {
             return string.Join(separator, values);
+        }
+        
+        [ExecutableFunction(ExecuteInDependency = true), CeresLabel("Format String")]
+        public static string Flow_StringFormat(string separator, object[] values)
+        {
+            return string.Format(separator, values);
+        }
+        
+        [ExecutableFunction(IsSelfTarget = true, ExecuteInDependency = true), CeresLabel("Replace String")]
+        public static string Flow_StringReplace(string stringValue, string oldValue, string newValue)
+        {
+            return stringValue.Replace(oldValue, newValue);
+        }
+        
+        [ExecutableFunction(IsSelfTarget = true, ExecuteInDependency = true), CeresLabel("ToLower")]
+        public static string Flow_StringToLower(string stringValue)
+        {
+            return stringValue.ToLower(CultureInfo.CurrentCulture);
+        }
+        
+        [ExecutableFunction(IsSelfTarget = true, ExecuteInDependency = true), CeresLabel("ToUpper")]
+        public static string Flow_StringToUpper(string stringValue)
+        {
+            return stringValue.ToUpper(CultureInfo.CurrentCulture);
+        }
+        
+        [ExecutableFunction(IsSelfTarget = true, ExecuteInDependency = true), CeresLabel("Hash64")]
+        public static ulong Flow_StringHash64(string stringValue)
+        {
+            return stringValue.Hash64();
         }
     }
 }
