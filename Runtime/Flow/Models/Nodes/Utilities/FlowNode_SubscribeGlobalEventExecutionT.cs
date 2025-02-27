3e55804546abf3a4491c194156e791456446e841
AkiKurisu <2683987717@qq.com>
2025-02-27 17:09:05 +0800

    Support subscribe global event

---
+++ Runtime/Flow/Models/Nodes/Utilities/FlowNode_SubscribeGlobalEventExecutionT.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Ceres.Annotations;
+using Chris.Events;
+
+namespace Ceres.Graph.Flow.Utilities
+{
+    [Serializable]
+    [CeresGroup("Utilities/Rx")]
+    [CeresLabel("Global Subscribe {0}")]
+    public class FlowNode_SubscribeGlobalEventExecutionT<TEventType>: FlowNode
+        where TEventType : EventBase<TEventType>, new()
+    {
+        [OutputPort]
+        public CeresPort<IDisposable> subscription;
+
+        protected override void LocalExecute(ExecutionContext executionContext)
+        {
+            if (executionContext.Context is not IFlowGraphRuntime runtime)
+            {
+                return;
+            }
+            subscription.Value = EventSystem.EventHandler.SubscribeExecution<TEventType>(runtime);
+        }
+    }
+}
---
+++ Runtime/Flow/Models/Nodes/Utilities/Templates/FlowNode_SubscribeGlobalEventExecutionT_Template.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Ceres.Graph.Flow.Annotations;
+using Ceres.Utilities;
+using Chris.Events;
+
+
+namespace Ceres.Graph.Flow.Utilities.Templates
+{
+    public class FlowNode_SubscribeGlobalEventExecutionT_Template: GenericNodeTemplate
+    {
+        public override bool RequirePort()
+        {
+            return false;
+        }
+        
+        public override Type[] GetGenericArguments(Type portValueType, Type selectArgumentType)
+        {
+            return new[] { selectArgumentType };
+        }
+
+        public override Type[] GetAvailableArguments(Type portValueType)
+        {
+            return SubClassSearchUtility.FindSubClassTypes(typeof(EventBase))
+                .Where(x => x.GetCustomAttribute<ExecutableEventAttribute>() != null).ToArray();
+        }
+    }
+}
