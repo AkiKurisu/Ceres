4b464bd64b2176f1c6519c8a500f05236980fe23
AkiKurisu <2683987717@qq.com>
2025-09-27 23:50:20 +0800

    Update CeresBuildProcessor.cs

--- Editor/Core/CeresBuildProcessor.cs
+++ Editor/Core/CeresBuildProcessor.cs
@@ -1,37 +1,21 @@
 ﻿using System.IO;
 using System.Linq;
+using Chris.Editor;
 using Chris.Serialization;
-using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 
 namespace Ceres.Editor
 {
-    public class CeresBuildProcessor : IPreprocessBuildWithReport, IPostprocessBuildWithReport
+    public class CeresBuildProcessor : BuildProcessorWithReport
     {
-        public int callbackOrder { get; }
-
         private readonly LinkXmlGenerator _linker = LinkXmlGenerator.CreateDefault();
 
         private static readonly string CeresDirectory = Path.Combine(Application.dataPath, "Ceres");
-        
+
         private static readonly string XMLPath = Path.Combine(CeresDirectory, "link.xml");
 
-        public void OnPostprocessBuild(BuildReport report)
-        {
-            if (File.Exists(XMLPath))
-            {
-                File.Delete(XMLPath);
-                File.Delete(XMLPath + ".meta");
-            }
-            if (Directory.Exists(CeresDirectory))
-            {
-                Directory.Delete(CeresDirectory);
-                File.Delete(CeresDirectory + ".meta");
-            }
-        }
-
-        public void OnPreprocessBuild(BuildReport report)
+        protected override void PreprocessBuild(BuildReport report)
         {
             var preservedTypes = CeresSettings.GetPreservedTypes();
             _linker.AddTypes(preservedTypes.Select(SerializedType.FromString));
@@ -39,7 +23,23 @@ namespace Ceres.Editor
             {
                 Directory.CreateDirectory(CeresDirectory);
             }
+
             _linker.Save(XMLPath);
+        }
+
+        protected override void PostprocessBuild(BuildReport report)
+        {
+            if (File.Exists(XMLPath))
+            {
+                File.Delete(XMLPath);
+                File.Delete(XMLPath + ".meta");
+            }
+
+            if (Directory.Exists(CeresDirectory))
+            {
+                Directory.Delete(CeresDirectory);
+                File.Delete(CeresDirectory + ".meta");
+            }
         }
     }
 }
